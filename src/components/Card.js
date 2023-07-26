5674327e505182d973bb4fa0180a23f168a9292a
Gabriel Menendez <menendez.gabriel@gmail.com>
2023-07-26 19:25:53 -0300

    add image when is cold

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Player } from "@lottiefiles/react-lottie-player";
 const Card = () => {
   const [weatherData, setWeather] = useState([]);
   const [messageData, setMessage] = useState([]);
@@ -18,8 +19,6 @@ const Card = () => {
   });
 
   const messageHandler = (weatherData) => {
-    console.log("acaaaa");
-    console.log(weatherData);
     switch (true) {
       case weatherData > 15:
         setMessage(
@@ -46,6 +45,27 @@ const Card = () => {
       <div className="container-fluid py-5">
         <h1 className="display-5 fw-bold">Hace frio en Hurlingham City?</h1>
         <p>{messageData}</p>
+        {/* <Player
+          src="https://lottie.host/5d93967b-6f17-45ea-8436-de01f5bade72/bRAg9R530M.json"
+          className="player"
+          loop
+          autoplay
+          style={{ height: "300px", width: "300px" }}
+        /> */}
+        {/* <Player
+          src="https://lottie.host/9013b8af-a7b5-4656-bf49-67524a1ec69f/7LivCbU2bF.json"
+          className="player"
+          loop
+          autoplay
+          style={{ height: "300px", width: "300px" }}
+        /> */}
+        <Player
+          src="https://lottie.host/c17f9aa0-db80-40c0-8afb-d618deb0a8aa/lFTgAnOhYA.json"
+          className="player"
+          loop
+          autoplay
+          style={{ height: "300px", width: "300px" }}
+        />
       </div>
     </div>
   );
