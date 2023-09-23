9e0957bc0820a8d32aebc6294ea9fb8f87432d3c
Gabriel Menendez <menendez.gabriel@gmail.com>
2023-09-23 15:40:26 -0300

    Added new efects

--- src/Pages/SpainOnePage.js
+++ src/Pages/SpainOnePage.js
@@ -5,23 +5,23 @@ const SpainOnePage = (props) => {
   const [weatherData, setWeatherData] = useState([]);
   const [spainTime, setSpainTime] = useState({})
   const Toledo =
-    "https://api.weatherapi.com/v1/current.json?key=8da8abb59d604218b0a15508232807&q=Toledo, castilla la mancha, spain";
+    "https://api.weatherapi.com/v1/current.json?key=8da8abb59d604218b0a15508232807&q=Toledo,castilla la mancha,spain&lang=es";
   const Jaen =
-    "https://api.weatherapi.com/v1/current.json?key=8da8abb59d604218b0a15508232807&q=Jaen, Andalucia, spain";
+    "https://api.weatherapi.com/v1/current.json?key=8da8abb59d604218b0a15508232807&q=Jaen,Andalucia,spain&lang=es";
   const Alhambra =
-    "https://api.weatherapi.com/v1/current.json?key=8da8abb59d604218b0a15508232807&q=Alhambra, andalucia ,spain";
+    "https://api.weatherapi.com/v1/current.json?key=8da8abb59d604218b0a15508232807&q=Alhambra,andalucia,spain&lang=es";
   const Ubeda =
-    "https://api.weatherapi.com/v1/current.json?key=8da8abb59d604218b0a15508232807&q=ubeda,andalucia,spain";
+    "https://api.weatherapi.com/v1/current.json?key=8da8abb59d604218b0a15508232807&q=ubeda,andalucia,spain&lang=es";
   const Malaga =
-    "https://api.weatherapi.com/v1/current.json?key=8da8abb59d604218b0a15508232807&q=malaga, andalucia , spain";
+    "https://api.weatherapi.com/v1/current.json?key=8da8abb59d604218b0a15508232807&q=malaga,andalucia,spain&lang=es";
   const Olvera =
-    "https://api.weatherapi.com/v1/current.json?key=8da8abb59d604218b0a15508232807&q=olvera, andalucia ,spain";
+    "https://api.weatherapi.com/v1/current.json?key=8da8abb59d604218b0a15508232807&q=olvera,andalucia,spain&lang=es";
   const Sevilla =
-    "https://api.weatherapi.com/v1/current.json?key=8da8abb59d604218b0a15508232807&q=sevilla, andalucia ,spain";
+    "https://api.weatherapi.com/v1/current.json?key=8da8abb59d604218b0a15508232807&q=sevilla,andalucia,spain&lang=es";
   const Barcelona =
-    "https://api.weatherapi.com/v1/current.json?key=8da8abb59d604218b0a15508232807&q=barcelona, catalonia, spain";
+    "https://api.weatherapi.com/v1/current.json?key=8da8abb59d604218b0a15508232807&q=barcelona,catalonia,spain&lang=es";
   const Madrid =
-    "https://api.weatherapi.com/v1/current.json?key=8da8abb59d604218b0a15508232807&q=madrid, madrid, spain";
+    "https://api.weatherapi.com/v1/current.json?key=8da8abb59d604218b0a15508232807&q=madrid,madrid,spain&lang=es";
   const cities = [
     {city: "Toledo", url:Toledo},
     {city: "Jaen", url:Jaen},
@@ -42,6 +42,8 @@ const SpainOnePage = (props) => {
         // const data = response.json()
         setWeatherData(weatherData => [...weatherData,{
           temperature: data.current.temp_c,
+          icon: data.current.condition.icon,
+          condition: data.current.condition.text,
           city: city,
         }]);
       });
--- src/components/WeatherSpan.js
+++ src/components/WeatherSpan.js
@@ -1,21 +1,38 @@
+import "./styles/WeatherSpanStyle.css"
+
 const WeatherSpan = (props) => {
   const temperatures = props.data;
-  const time = props.dateTime.time
+  const time = props.dateTime.time;
+  console.log(temperatures);
   return (
     <div>
       <p>España</p>
-
       {temperatures.map((temperature) => (
-        <div key={Math.floor(Math.random() * 10000)} className="border-info m-2 card">
-          <div className="card-header">
-            Spain - Hora: {time}
-          </div>
-          <div className="card-body">
-            <h5 className="card-title">Ciudad: {temperature.city}</h5>
-            <p>Temperatura: {temperature.temperature}°</p>
+        <div
+          key={Math.floor(Math.random() * 10000)}
+          className="border-info m-2 card mb-3"
+        >
+          <div className="row g-0">
+            <div className="col-md-4 border-bottom">
+              <img src={temperature.icon} className=" p-2 img-fluid rounded-start card_image" />
+            </div>
+            <div className="col-md-8">
+              <div className="card-body">
+                <h5 className="card-title">Ciudad: {temperature.city}</h5>
+                <p className="card-text">
+                  Temperatura: {temperature.temperature}°
+                </p>
+                <p className="card-text">Condition: {temperature.condition}</p>
+                <p className="card-text">Spain - Hora: {time}</p>
+              </div>
+            </div>
           </div>
         </div>
       ))}
+      Powered by{" "}
+      <a href="https://www.weatherapi.com/" title="Free Weather API">
+        WeatherAPI.com
+      </a>
     </div>
   );
 };
